59dc5103dfe2674983e091f0efe3b5fd48b63bbf
Igor Tkanov <tkanoff@gmail.com>
2018-01-15 15:34:05 +0100

    do no replacements in capistrano-db-tasks, do it in a rake task in the main project

--- lib/capistrano-db-tasks/database.rb
+++ lib/capistrano-db-tasks/database.rb
@@ -185,14 +185,10 @@ module Database
     end
 
     def remove_sensitive_data
-      puts "Local database: remove sensitive data"
-
-      schemas = %w(public lab us abstract)
-      schemas.each do |schema|
-        puts "Local database: removing sensitive data from schema #{schema}"
-        email_update_sql = "UPDATE #{schema}.users SET email = CONCAT(SUBSTRING(email from '^[\\w.+\\-\\_\\~\\d]+'), '@bogus.ams-ix.net') WHERE email not like '%ams-ix.net';"
-        execute("#{pgpass} psql -d #{database} #{credentials} -c \"#{email_update_sql}\"")
-      end
+      puts "Local database: removing sensitive data, hold tight..."
+      stdout3, status3 = Open3.capture2('bundle', 'exec', 'rake', 'replace_emails_in_database')
+      puts stdout3
+      puts status3
     end
 
     private
